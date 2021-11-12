7b2a8b4cf327592e985f0987baa83cc6bf2fb176
myifengs <myifengs@gmail.com>
2021-11-12 21:01:28 +0800

    init

---
+++ src/test/java/com/myifeng/common/MybatisOperationLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myifeng.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisOperationLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
